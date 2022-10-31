7b27801975d9aba4c84d7c60f6f00f3c11e818b4
lokesh-mali <mali.lokesh5050@gmail.com>
2022-10-31 18:42:54 +0530

    ok

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,5 +9,15 @@ weatherForm.addEventListener("submit"  , async(e)=>{
     const locationKey = cityData.data[0].Key
    
     const weather = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=a1Aa7JCSzw4koBXV0wWHZ76vUDHvQB0n&language=en-us&details=true&metric=true`)
-    console.log(weather.data);
+    console.log(weather.data.DailyForecasts);
+    let min_temp = weather.data.DailyForecasts[0].Temperature.Minimum.Value
+    let max_temp = weather.data.DailyForecasts[0].Temperature.Maximum.Value
+    let date = weather.data.DailyForecasts[0].Date.split("T")[0]
+    let time = new Date().toLocaleTimeString()
+    let windGust
+    console.log(time);
+    console.log(date);
+    console.log(min_temp);
+
+    
 })

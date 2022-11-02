f9c416bd4c9cee3a59b9a127d186884d5c63cdc9
lokesh-mali <mali.lokesh5050@gmail.com>
2022-11-02 12:07:39 +0530

    💖

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors({ origin: process.env.DOMAIN  , credentials:true}));
+app.use(cors({ origin: process.env.DOMAIN , credentials:true}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
